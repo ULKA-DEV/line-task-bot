9c5bdb7817f57701c0d46677f78ad90b6ee6efd3
ULKA-DEV <info@ulka.co.th>
2026-06-06 08:40:28 +0700

    bot.py

--- bot.py
+++ bot.py
@@ -297,7 +297,8 @@ def index():
     return "LINE Task Bot is running! 🤖"
 
 
+init_db()
+
 if __name__ == "__main__":
-    init_db()
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
